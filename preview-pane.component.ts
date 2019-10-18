2072985dd716f9033943d691637034ddb4aafce6
daniellehenry4996 <55170228+daniellehenry4996@users.noreply.github.com>
2019-10-17 20:49:19 -0400

    Add files via upload

---
+++ preview-pane.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-pane',
+  templateUrl: './preview-pane.component.html',
+  styleUrls: ['./preview-pane.component.css']
+})
+export class PreviewPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    
+
+}
