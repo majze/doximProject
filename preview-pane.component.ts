6d1c039015808a600322ded1adf0aa01e471bafc
daniellehenry4996 <55170228+daniellehenry4996@users.noreply.github.com>
2019-10-22 13:24:00 -0400

    Add files via upload

--- preview-pane.component.ts
+++ preview-pane.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import * as jsPDF from 'jspdf';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-preview-pane',
   templateUrl: './preview-pane.component.html',
   styleUrls: ['./preview-pane.component.css']
 })
-export class PreviewPaneComponent implements OnInit {
+export class PreviewPaneComponent {
 
-  constructor() { }
+ public captureScreen()
+ {
+   var data = document.getElementById('a');
+   html2canvas(data).then(canvas =>{
 
-  ngOnInit() {
-  }
-
-downloadPDF()
-
-{
-  const doc = new jsPDF();
-  doc.text('hello',15, 15);
-  doc.save('first.pdf');
-}
+    var imgWidth =200;
+    var pageHeight =295;
+    var imgHeight = canvas.height * imgWidth / canvas.width;
+    var heightLeft = imgHeight;
+    const contentDataURL = canvas.toDataURL('image/png')
+    let pdf = new jspdf('p','mm','a4');
+    var position =0;
+    pdf.addImage(contentDataURL, 'PNG', 0, position,imgWidth, imgHeight)
+    pdf.save('PDF.pdf');
+    
+   });
+   
+ }
 
 }
