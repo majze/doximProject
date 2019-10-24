10c0bfceda9a3c9ab802376535a6b68361384dbc
dspreder <donald.spreder@gmail.com>
2019-10-24 17:41:03 -0400

    local function populates view pane

--- src/app/build-page/preview-pane/preview-pane.component.ts
+++ src/app/build-page/preview-pane/preview-pane.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+
+function populateSymitarCC() {
+  let logoSectionBuffer:HTMLElement = document.getElementsByClassName("logoSection")[0] as HTMLElement;
+  logoSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/defaultLogo.png)";
+
+  let addressSectionBuffer:HTMLElement = document.getElementsByClassName("addressSection")[0] as HTMLElement;
+  addressSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/address.png)";
+  
+  let ccLogoSectionBuffer:HTMLElement = document.getElementsByClassName("ccLogoSection")[0] as HTMLElement;
+  ccLogoSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccVisaLogo.png)";
+
+  let headerSectionBuffer:HTMLElement = document.getElementsByClassName("headerSection")[0] as HTMLElement;
+  headerSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccHeader.png)";
+
+  let topGraphicSectionBuffer:HTMLElement = document.getElementsByClassName("topGraphicSection")[0] as HTMLElement;
+  topGraphicSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccTopGraphic.png)";
+
+  let ccMidSectionBuffer:HTMLElement = document.getElementsByClassName("ccMidSection")[0] as HTMLElement;
+  ccMidSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccSymMidWithAll.png)";
+
+  let couponSectionBuffer:HTMLElement = document.getElementsByClassName("couponSection")[0] as HTMLElement;
+  couponSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccSymCoupon.png)";
+
+  let scanlineSectionBuffer:HTMLElement = document.getElementsByClassName("scanlineSection")[0] as HTMLElement;
+  scanlineSectionBuffer.style.backgroundImage="url(../../../assets/ccSymitar/ccScanline.png)";
+}
 
 @Component({
   selector: 'app-preview-pane',
@@ -6,15 +33,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-pane.component.css']
 })
 export class PreviewPaneComponent implements OnInit {
+  activeCore: string;
+  activeStatementType: string;
 
   constructor() { }
 
-  // populateSkeleton() {
-  //   let el = document.getElementsByClassName("logoSection").style.backgroundImage="url(../../../assets/ccSymitar/ccSymLogo.png)";
-
-  // }
+  popSkele(){
+    if (this.activeCore == 'symitar' && this.activeStatementType == 'creditCard') {
+      populateSymitarCC();
+    }
+    else
+    {
+      console.log("popSkele: Assets not loaded, core and statementType not loaded")
+    }
+  }  
 
   ngOnInit() {
+    // DON: This temp code will be replaced
+    this.activeCore = 'symitar';
+    this.activeStatementType = 'creditCard';
   }
 
 }
